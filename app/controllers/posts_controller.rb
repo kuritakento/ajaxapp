c7a459b8b154b11308271419cf50c5cc5b653092
kuritakento <kento.kurita@di-v.co.jp>
2020-06-21 12:18:32 +0900

    既読機能の実装①

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,25 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order(id: "DESC")
   end
 
   def new
   end
 
   def create
-    Post.create(post_params)
+    Post.create(content: params[:content])
+    redirect_to action: :index
   end
 
-  private
-  def post_params
-    params.permit(:content)
+  def checked
+    post = Post.find(params[:id])
+    if post.checked
+      post.update(checked: false)
+    else
+      post.update(checked: true)
+    end
+
+    item = Post.find(params[:id])
+    render json: { post: item }
   end
 end
